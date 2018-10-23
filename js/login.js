0a4f186c70456a05c288c9535f27b50a65561248
Marvelous <phongpham.hedspi@gmail.com>
2018-10-23 16:50:03 +0700

    dm nguyen

--- js/login.js
+++ js/login.js
@@ -1,18 +1,19 @@
-$(document).ready(function() {
-	$('#login').on('submit', function(e) {
-		e.preventDefault();
-		var name = $('#username').val();
-		var pass = $('#password').val();
-		$.ajax({
-			url:'http://localhost:6969/api/auth/login',
-			method: 'POST',
-			data: {username: name, password: pass},
-			success: function(data) {
-				window.location.href = '/'; 
-			},
-			error: function() {
-				console.log("error");
-			} 
-		});
-	});
+$(document).ready(function () {
+    $('#login').on('submit', function (e) {
+        e.preventDefault();
+        var name = $('#username').val();
+        var pass = $('#password').val();
+        $.ajax({
+            url: 'http://localhost:6969/api/auth/login',
+            method: 'POST',
+            data: {username: name, password: pass},
+            success: function (data) {
+                window.location.href = '/';
+
+            },
+            error: function () {
+                console.log("error");
+            }
+        });
+    });
 });
--- js/register.js
+++ js/register.js
@@ -1,13 +1,56 @@
-// var password = document.getElementById("password")
-//   , confirm_password = document.getElementById("confirm_password");
+// var password, confirm_password;
 //
-// function validatePassword(){
-//   if(password.value != confirm_password.value) {
-//     confirm_password.setCustomValidity("Passwords Don't Match");
-//   } else {
-//     confirm_password.setCustomValidity('');
-//   }
+// $(document).ready(function () {
+//     password = $('#password').val();
+//     confirm_password = $('#confirm_password').val();
+//     $('#confirm_password').on('input', function (e) {
+//         validatePassword(password,confirm_password);
+//     });
+// });
+//
+// function validatePassword(password,confirm_password) {
+//     if (password !== confirm_password) {
+//         $('#confirm_password')[0].setCustomValidity("Passwords Don't Match");
+//     } else {
+//         $('#confirm_password')[0].setCustomValidity('');
+//     }
 // }
+// var password, confirm_password;
 //
-// password.onchange = validatePassword;
-// confirm_password.onkeyup = validatePassword;
+$(document).ready(function () {
+    // password = $('#password').val();
+    // confirm_password = $('#confirm_password').val();
+    $('#register').validate({
+        rules: {
+            username: {
+                required: true,
+                minlength: 6
+            },
+            password: {
+                required: true,
+                minlength: 6
+            },
+            confirm_password: {
+                required: true,
+                minlength: 6,
+                equalTo: "#password"
+            }
+        },
+        message: {
+            username: {
+                required: "Please input username",
+                minlength: "Your password must be at least 6 characters."
+            },
+            password: {
+                required: "Please input password",
+                minlength: "Your password must be at least 6 characters."
+            },
+            confirm_password: {
+                required: "Please confirm password",
+                minlength: "Your password must be at least 6 characters.",
+                equalTo: "Please enter the same password as above"
+            }
+        },
+        debug: true
+    });
+});
